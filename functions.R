2c50229bd18cade78302064057e42fd24466abca
bw4sz <benweinstein2010@gmail.com>
2019-08-01 20:55:43 -0700

    change image size

--- functions.R
+++ functions.R
@@ -43,7 +43,7 @@ renderGallery<-function(image_paths){
     fluidRow(
       lapply(selected_images, function(img) {
         column(4,offset = 0.5, 
-               tags$img(src=img, class="clickimg", 'data-value'=img, height="20%")
+               tags$img(src=img, class="clickimg", 'data-value'=img, height="100px",width="auto")
         )
       })
     )
