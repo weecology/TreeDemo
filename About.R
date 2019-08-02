17cadd05f5ac8083e05c8289ee0107e83f70cb11
bw4sz <benweinstein2010@gmail.com>
2019-08-01 20:26:05 -0700

    need to figure out image sizing in gallery

--- About.R
+++ About.R
@@ -5,7 +5,7 @@ about_page<-function(){
       titlePanel("About this project"),
       p(style="font-size:25px", "For more information on this project, including details of model training and performance, see",
         a("Weinstein, B. G., Marconi, S., Bohlman, S., Zare, A., & White, E. (2019). Individual tree-crown detection in RGB imagery using semi-supervised deep learning neural networks. Remote Sensing, 11(11), 1309.",href="https://www.mdpi.com/2072-4292/11/11/1309")),
-      p(style="font-size:25px", "The goal of this project is to provide RGB tree detection at broad spatial scales. The goal is not to replace traditional ground survey techniques, or suggest they can be replaced by automated approaches. While the segmentations are not perfect, the aim is to provide an opportunity to scale beyond traditional plot level measures. Whether this best fits your scientific question depends on the sensitivity of your data to segmentation results."),
+      p(style="font-size:25px", "The goal of this project is to provide RGB tree detection at broad spatial scales. The goal is not to replace traditional ground survey techniques or suggest they can be replaced by automated approaches. While the segmentations are not perfect, the aim is to provide an opportunity to scale beyond traditional plot level measures. Whether this fits your scientific question depends on the sensitivity of your data to segmentation results."),
       h1("Ben Weinstein"),
       img(src="ben-weinstein.jpg",height="300px"),
       p(style = "font-size:25px","I am a researcher in the White Lab at the University of Florida. I study ecology, evolution and the use of computer vision for biological monitoring."),
