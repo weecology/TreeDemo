560cef63666da31fa1f45865597eafff8ebbc637
bw4sz <benweinstein2010@gmail.com>
2019-08-01 21:01:40 -0700

    image grid sizing

--- explore.R
+++ explore.R
@@ -12,6 +12,10 @@ side_panel<-sidebarPanel(
   leafletOutput("map"),
   h2("Select an image"),
   #custom defined UI function
+  tags$head(tags$style(
+    type="text/css",
+    "#image img {max-width: 100%; width: 100%; height: auto}"
+  )),
   uiOutput("imageGrid"),
   tags$script(HTML(
     "$(document).on('click', '.clickimg', function() {",
