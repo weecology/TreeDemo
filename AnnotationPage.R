3084e85ce8cb250871f4798b7fa6a465047c6ae2
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-04 13:41:52 -0800

    move UI slider to bottom

--- AnnotationPage.R
+++ AnnotationPage.R
@@ -13,6 +13,7 @@ AnnotationPage<-function(){
       titlePanel("Annotation Environment"),
       p("The National Ecological Observatory Network collects airborne and forestry data across the United States. Our goal is to turn those surveys into ecological information on individual trees.To do that we must build and validate model of tree detection and classification. This page provides supplamental information for the Tree Crown Detection Zooniverse project"),
       selectizeInput("annotation_plotID", "plotID", plotIDs, selected = "SJER_052.tif", multiple = FALSE,options = NULL),
+      leafletOutput("annotation_hsi",height="700"),
       fluidRow(
       # Input: Simple integer interval ----
       column(1,sliderInput("HSI_band_1", "HSI band 1:",
@@ -29,7 +30,6 @@ AnnotationPage<-function(){
                   min = 0, max = 369,
                   value = 113))),
       
-      leafletOutput("annotation_hsi",height="700"),
       rglwidgetOutput("annotation_lidar",height='500')
       )})
 }
