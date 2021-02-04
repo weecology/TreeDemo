95d22ae543da359775dc2c647a54afdd24a44eec
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-04 13:53:10 -0800

    side bar panel

--- AnnotationPage.R
+++ AnnotationPage.R
@@ -11,26 +11,27 @@ AnnotationPage<-function(){
   renderUI({
     fluidPage(
       titlePanel("Annotation Environment"),
-      p("The National Ecological Observatory Network collects airborne and forestry data across the United States. Our goal is to turn those surveys into ecological information on individual trees.To do that we must build and validate model of tree detection and classification. This page provides supplamental information for the Tree Crown Detection Zooniverse project"),
+      p("The National Ecological Observatory Network collects airborne and forestry data across the United States. Our goal is to turn those surveys into ecological information on individual trees. To do that we must build and validate models of tree crown detection and species classification. This page provides information for the Tree Crown Detection Zooniverse project to assist in ongoing image annotation."),
+      sidebarPanel(width=2,
       selectizeInput("annotation_plotID", "plotID", plotIDs, selected = "SJER_052.tif", multiple = FALSE,options = NULL),
-      leafletOutput("annotation_hsi",height="700"),
-      fluidRow(
       # Input: Simple integer interval ----
-      column(1,sliderInput("HSI_band_1", "HSI band 1:",
+      sliderInput("HSI_band_1", "HSI band 1:",
                   min = 0, max = 369,
-                  value = 11)),
+                  value = 11),
       
       # Input: Decimal interval with step value ----
-      column(2,sliderInput("HSI_band_2", "HSI band 2:",
+      sliderInput("HSI_band_2", "HSI band 2:",
                   min = 0, max = 369,
-                  value = 55)),
+                  value = 55),
       
       # Input: Specification of range within an interval ----
-      column(3,sliderInput("HSI_band_3", "HSI band 3:",
+      sliderInput("HSI_band_3", "HSI band 3:",
                   min = 0, max = 369,
-                  value = 113))),
+                  value = 113)),
       
-      rglwidgetOutput("annotation_lidar",height='600')
-      )})
+      mainPanel(
+      leafletOutput("annotation_hsi",height="700"),
+      rglwidgetOutput("annotation_lidar",height='700')
+      ))})
 }
 
