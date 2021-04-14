289ef059c2b95eb1efb1b0018b05884512e0bab6
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-04-14 15:01:38 -0700

    local tests pass

--- Zooniverse/create_species_dataset.py
+++ Zooniverse/create_species_dataset.py
@@ -34,10 +34,12 @@ def sample_plots(shp):
     test = shp[shp.plotID.isin(test_plots)]
     train = shp[~shp.plotID.isin(test_plots)]
     
-    test = test.groupby("taxonID").filter(lambda x: x.shape[0] > 5)
+    test = test.groupby("label").filter(lambda x: x.shape[0] > 5)
     
-    train = train[train.taxonID.isin(test.taxonID)]
-    test = test[test.taxonID.isin(train.taxonID)]
+    train = train[train.label.isin(test.label)]
+    test = test[test.label.isin(train.label)]
+    
+    return train, test
     
 def split_train_test(annotations, client, iterations=1):
     """Split shapefile into balanced train and test"""
@@ -52,32 +54,32 @@ def split_train_test(annotations, client, iterations=1):
         
         for x in as_completed(futures):
             train, test = x.result()
-            if len(train.taxonID.unique()) > most_species:
-                print(len(train.taxonID.unique()))
+            if len(train.label.unique()) > most_species:
+                print(len(train.label.unique()))
                 saved_train = train
                 saved_test = test
-                most_species = len(test.taxonID.unique())            
+                most_species = len(test.label.unique())            
     else:
         for x in np.arange(iterations):
             train, test = sample_plots(shp=annotations)
-            if len(train.taxonID.unique()) > most_species:
-                print(len(test.taxonID.unique()))
+            if len(train.label.unique()) > most_species:
+                print(len(test.label.unique()))
                 saved_train = train
                 saved_test = test
-                most_species = len(test.taxonID.unique())
+                most_species = len(test.label.unique())
     
     train = saved_train
     test = saved_test     
     
     print("There are {} records for {} species for {} sites in filtered train".format(
         train.shape[0],
-        len(train.taxonID.unique()),
+        len(train.label.unique()),
         len(train.siteID.unique())
     ))
     
     print("There are {} records for {} species for {} sites in test".format(
         test.shape[0],
-        len(test.taxonID.unique()),
+        len(test.label.unique()),
         len(test.siteID.unique())
     ))
     
@@ -90,10 +92,11 @@ def run(input_dir, client, save_dir, iterations=1):
     train.to_csv("{}/train.csv".format(save_dir))
     test.to_csv("{}/test.csv".format(save_dir))
     
+    return train, test
 
 if __name__ == "__main__":
     client = start_cluster.start(cpus=20)
-    run(input_dir="/orange/idtrees-collab/DeepTreeAttention/data/", savedir="/orange/idtrees-collab/DeepTreeAttention/data/", client=client, iterations=500)
+    run(input_dir="/orange/idtrees-collab/DeepTreeAttention/data/", savedir="/orange/idtrees-collab/DeepTreeAttention/data/", client=client, iterations=1000)
     
 
     
--- Zooniverse/tests/test_create_dataset.py
+++ Zooniverse/tests/test_create_dataset.py
@@ -3,13 +3,13 @@ import create_species_dataset
 
 def test_load_shapefile():
     df = create_species_dataset.load_shapefiles("data")
-    assert all([x in ["xmin","ymin","xmax","ymax","label"] for x in df.columns])
+    assert all([x in df.columns for x in ["xmin","ymin","xmax","ymax","label"]])
     
 def test_run(tmpdir):
     train, test = create_species_dataset.run(input_dir="data", client=None, save_dir=tmpdir, iterations=3)
     
     assert not train.empty
     assert not test.empty
-    assert all([x in ["xmin","ymin","xmax","ymax","label"] for x in train.columns])
-    assert all([x in ["xmin","ymin","xmax","ymax","label"] for x in test.columns])
+    assert all([x in train.columns for x in ["xmin","ymin","xmax","ymax","label"]])
+    assert all([x in test.columns for x in ["xmin","ymin","xmax","ymax","label"]])
     assert train[train.plotID.isin(test.plotID)].empty
